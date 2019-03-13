330d8da8daba705633496a2ba0cf605b1fa3ebe2
BrannanC <brannanconrad@gmail.com>
2019-03-12 21:44:56 -0500

    Nullabel for real now

--- data/migrations/20190310153257_users_table.js
+++ data/migrations/20190310153257_users_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', tbl => {
         tbl.increments();
-        tbl.string('name', 128).notNullable();        
+        tbl.string('name', 128);        
         tbl.string('username', 128).unique().notNullable();    
         tbl.string('email', 128).unique().notNullable();   
         tbl.string('password', 255).notNullable();
